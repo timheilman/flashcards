dee89c32cfa08ecc673f3210068822865b35ffb2
Tim Heilman <tim@dv8.org>
2016-10-25 07:18:25 -0700

    Eliminate all IDE and compiler warnings on this file.

--- flashcard-processing/src/main/java/crosswords/flashcards/domain/entry/impl/AnnotatedEntryImpl.java
+++ flashcard-processing/src/main/java/crosswords/flashcards/domain/entry/impl/AnnotatedEntryImpl.java
@@ -2,26 +2,33 @@ package crosswords.flashcards.domain.entry.impl;
 
 import com.google.inject.Inject;
 import com.google.inject.assistedinject.Assisted;
-import crosswords.flashcards.domain.*;
-import crosswords.flashcards.factories.bindingannotations.*;
+import crosswords.flashcards.domain.AnnotatedEntry;
+import crosswords.flashcards.domain.Entry;
+import crosswords.flashcards.domain.Inflection;
+import crosswords.flashcards.domain.Inflections;
+import crosswords.flashcards.factories.bindingannotations.Enable1;
+import crosswords.flashcards.factories.bindingannotations.Otcwl2;
+import crosswords.flashcards.factories.bindingannotations.Otcwl2014TwoToEightLetterWords;
+import crosswords.flashcards.factories.bindingannotations.WordsWithFriends4;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 
 public class AnnotatedEntryImpl implements AnnotatedEntry {
-    public static final int MAX_LENGTH_OF_KNOWN_OTCWL2014_MEMBERSHIP = 8;
+    private static final int MAX_LENGTH_OF_KNOWN_OTCWL2014_MEMBERSHIP = 8;
     protected String entryWord;
     protected final Entry entry;
-    protected final Set<String> enable1;
-    protected final Set<String> wwf4;
-    protected final Set<String> o2;
-    protected final Set<String> o3_2_to_8;
+    private final Set<String> enable1;
+    private final Set<String> wwf4;
+    private final Set<String> o2;
+    private final Set<String> o3_2_to_8;
 
     @Inject
     // todo: comb code for IDE warnings and eliminate them systematically: go all-green
     // todo: test coverage in build system
     // todo: get test coverage to ~100%
+        // todo: get sowpods out of git; have build process start from binary
     // todo: log-scale file length histogram as part of pre-commit hooks? build?
     // todo: eliminate long data lists in code
     // todo: separate value objects from service objects; what's done in formatForPrinting should be done in a factory
@@ -29,12 +36,11 @@ public class AnnotatedEntryImpl implements AnnotatedEntry {
     // todo: feature-add sowpods
     // todo: feature-make cloze cards for inflection categories
     // todo: feature-heuristic to include ABAMPS in sowpods_dict_words: check other whole-word inflections for the missing S
-
-    public AnnotatedEntryImpl(@Enable1 Set<String> e,
-                              @WordsWithFriends4 Set<String> w,
-                              @Otcwl2 Set<String> o2,
-                              @Otcwl2014TwoToEightLetterWords Set<String> o3_2_to_8,
-                              @Assisted Entry entry) {
+    AnnotatedEntryImpl(@Enable1 Set<String> e,
+                       @WordsWithFriends4 Set<String> w,
+                       @Otcwl2 Set<String> o2,
+                       @Otcwl2014TwoToEightLetterWords Set<String> o3_2_to_8,
+                       @Assisted Entry entry) {
         enable1 = e;
         wwf4 = w;
         this.o2 = o2;
@@ -43,7 +49,6 @@ public class AnnotatedEntryImpl implements AnnotatedEntry {
         this.entry = entry;
     }
 
-    @Override
     public String formatForPrinting() {
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append(entryWord).append('\t');
@@ -55,7 +60,7 @@ public class AnnotatedEntryImpl implements AnnotatedEntry {
         return stringBuilder.toString();
     }
 
-    protected String getMembershipBlock(String perhapsMember, String prefix) {
+    String getMembershipBlock(String perhapsMember, String prefix) {
         StringBuilder potentiallyUnknownMembershipFlags = new StringBuilder();
         // we know for certain regarding otcwl2 (o2)
         if (o2.contains(perhapsMember)) {
@@ -73,7 +78,7 @@ public class AnnotatedEntryImpl implements AnnotatedEntry {
                 potentiallyUnknownMembershipFlags.toString());
     }
 
-    protected String getPartOfSpeechBlock() {
+    String getPartOfSpeechBlock() {
         return entry.getPartOfSpeech() == null ? null : "_" + entry.getPartOfSpeech().getDisplayString() + "_";
     }
 
@@ -94,7 +99,7 @@ public class AnnotatedEntryImpl implements AnnotatedEntry {
         return stringBuilder.toString();
     }
 
-    protected String getDefinitionBlock() {
+    String getDefinitionBlock() {
         String definition = entry.getDefinition();
         return definition == null ? "[no defn]" : definition;
     }
