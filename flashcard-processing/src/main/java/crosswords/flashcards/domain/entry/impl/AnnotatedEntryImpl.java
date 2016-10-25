bdbfedcc415bf623931697baa9715e75e395b2aa
Tim Heilman <tim@dv8.org>
2016-10-25 08:01:25 -0700

    new todo.

--- flashcard-processing/src/main/java/crosswords/flashcards/domain/entry/impl/AnnotatedEntryImpl.java
+++ flashcard-processing/src/main/java/crosswords/flashcards/domain/entry/impl/AnnotatedEntryImpl.java
@@ -28,6 +28,7 @@ public class AnnotatedEntryImpl implements AnnotatedEntry {
         // todo: get sowpods out of git; have build process start from binary
         // todo: idea-reformat all java files
         // todo: pre-commit hook caps files at 500 lines
+        // todo: get all compiler and IDEA warnings cleared: run 100% green
         // todo: get test coverage to ~100%
         // todo: starting from largest files to smallest, get all files well under 500 lines
         // todo: separate value objects from service objects; what's done in formatForPrinting should be done in a factory
